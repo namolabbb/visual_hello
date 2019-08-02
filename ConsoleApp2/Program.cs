6aed7e2e5700ef0f97405207bbef728c89a49a7e
egoing <egoing@gmail.com>
2019-08-03 03:07:26 +0900

    DDASD

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("World!");
+            Console.WriteLine("WGSFSD");
 
         }
     }
