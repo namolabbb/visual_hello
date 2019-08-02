3ef1016f82e44a34eb8882871c9426efc264dc3b
egoing <egoing@gmail.com>
2019-08-03 03:06:46 +0900

    SAD

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,6 +12,8 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("World!");
+            Console.WriteLine("WEQED");
+
 
         }
     }
